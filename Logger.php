6acee9594da9286c839ff40d0e60ef72e609f596
Hugo Moragues <hugo.moragues@gmail.com>
2017-11-30 22:16:06 +0100

    mq

--- Logger.php
+++ Logger.php
@@ -12,3 +12,36 @@ class TerminalLogger implements Logger
 		var_dump($message);
 	}
 }
+
+class BrowserLogger implements Logger
+{
+	public function log($message)
+	{
+		return '<pre>' . htmlentities($message) . '</pre>';
+	}
+}
+
+class Application
+{
+	protected $logger;
+
+	public function setLogger(Logger $logger)
+	{
+		$this->logger = $logger;
+
+		return $this;
+	}
+
+	public function action()
+	{
+		$this->logger->log('Doing things');
+	}
+}
+
+$app = new Application;
+$app->setLogger(new TerminalLogger);
+$app->action();
+
+$appWeb = new Application;
+$appWeb->setLogger(new BrowserLogger);
+$appWeb->action();
